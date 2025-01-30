a0016780785454c609761f856789564ad79824a7
Sontara444 <sontararajput@gmail.com>
2025-01-31 00:30:04 +0530

    list update

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,7 +1,32 @@
 import './List.css'
+
+
 const List = () => {
+
+
   return (
-    <div>List</div>
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+           <b>Image</b>
+           <b>Name</b>
+           <b>Category</b>
+           <b>Price</b>
+           <b>Action</b>
+        </div>
+        {/* use ,ap */}
+        <div className="list-table-format" >
+                <img src="" alt="" />
+                <p>X</p>
+                <p>X</p>
+                <p>X</p>
+                <p>X</p>
+                <p className='curser'>X</p>
+                
+              </div>
+      </div>
+    </div>
   )
 }
 
